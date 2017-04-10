242c0dd360679c21cec6a88ba79d7934d907c19a
kilon <kilon.alios@gmail.com>
2017-04-10 17:54:05 +0300

    add webhook for mailing-lists

--- secret.py
+++ secret.py
@@ -1,5 +1,6 @@
 token='Mjc4NjIwMjU0MzY2NTk3MTIw.C3yDWQ.QFKkNIB_z2P3sB3hJFbHyOVoTKY'
 webhGeneral = "https://discordapp.com/api/webhooks/279750728132591617/4OCpg06tC8mgywiv3xK9nCCZ_PaNaMVvy-zHFgtKhPLMVavxBFYGw0JpBooKvxfUbhFI"
 webhDevelopment = "https://discordapp.com/api/webhooks/278613672157511681/Y4pu7dcfeRrGQzOxtcrV0vVBwKEE7RyMDlz9F0uxiuKbwunGRvWKf3yPt2TFBGrAdOI5"
-channels={"lighthouse":"278848364710199296","general":"223421264751099906","development":"278558427796602882","offtopic":"278558544146464778","mailing-lists":"281076676614684672"}
+webhMailingLists ="https://discordapp.com/api/webhooks/301006312211218432/5YUyBUhZ92k0wFuF731OR2SQhb_E45WCDqSB3P00LdX2qYWmgKpkZ_tI73MAsfsRK0Rz"
+channels={"lighthouse":"278848364710199296","general":"223421264751099906","development":"278558427796602882","offtopic":"278558544146464778","mailing-lists":"301006312211218432"}
 
